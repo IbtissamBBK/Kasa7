5ad189be95b6c3454a1fdd46caf1de2f0e6f486f
Ibtissam <benbourekibtissam@gmail.com>
2024-11-27 22:57:57 +0100

    add data.json for carrousel + id appartment

--- kasa7-cra/src/App.jsx
+++ kasa7-cra/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 
+import data from "./components/data/data.json";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -19,7 +21,7 @@ function App() {
       <main className='container'>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/apartment/:id" element={<ApartmentDetails />} />
+          <Route path="/apartment/:id" element={<ApartmentDetails data={data} />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
